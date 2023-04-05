b49e0497560dd3d7c98f627999cec8f479e4430e
Helena Tenev <htenev14@gmail.com>
2023-04-04 23:02:54 -0400

    commit tree 11a6227a54e1973a50a86d539127cd28456b8f1a

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,14 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkElements = links.map((link) => {
+    return <a href={`#${link}`}>{link}</a>
+  });
+
+  return <nav>{/* display an <a> tag for each link here */linkElements}</nav>;
+  //for each string in the array, create an <a> tag that looks like this:
+  //<a href="#home">home</a>
+  //make sure each <a> element also gets a unique key prop
 }
 
 export default NavBar;
